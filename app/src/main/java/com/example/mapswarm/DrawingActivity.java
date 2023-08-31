3211de965d80775e8deb5fb88fe7e99bfa899e01
Wasura Wattearachchi <z5415828@ad.unsw.edu.au>
2023-08-31 10:41:20 +1000

    Add drawing saving to device for testing

--- app/src/main/java/com/example/mapswarm/DrawingActivity.java
+++ app/src/main/java/com/example/mapswarm/DrawingActivity.java
@@ -6,12 +6,22 @@ import static com.example.mapswarm.DrawingView.pathList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 
 public class DrawingActivity extends AppCompatActivity {
@@ -20,6 +30,7 @@ public class DrawingActivity extends AppCompatActivity {
     public static Paint paintBrush = new Paint();
     private Button pencil;
     private Button eraser;
+    private Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,22 +39,44 @@ public class DrawingActivity extends AppCompatActivity {
 
         pencil = findViewById(R.id.pencil);
         eraser = findViewById(R.id.eraser);
+        nextBtn = findViewById(R.id.nextBtn);
 
-        pencil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                paintBrush.setColor(Color.BLACK);
-                currentColour(paintBrush.getColor());
-            }
+        pencil.setOnClickListener(view -> {
+            paintBrush.setColor(Color.GREEN);
+            currentColour(paintBrush.getColor());
         });
 
-        eraser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                pathList.clear();
-                colourList.clear();
-                path.reset();
+        eraser.setOnClickListener(view -> {
+            pathList.clear();
+            colourList.clear();
+            path.reset();
+        });
+
+        nextBtn.setOnClickListener(view -> {
+            ContextWrapper cw = new ContextWrapper(getApplicationContext());
+            File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+            Long tsLong = System.currentTimeMillis()/1000;
+            String ts = tsLong.toString();
+            File file = new File(directory, ts +"UniqueFileName" + ".jpg");
+            if (!file.exists()) {
+                Log.d("path", file.toString());
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(file);
+                    View drawingView = findViewById(R.id.drawingView);
+                    drawingView.setDrawingCacheEnabled(true);
+                    Bitmap bitmap = drawingView.getDrawingCache();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                    fos.flush();
+                    fos.close();
+                } catch (java.io.IOException e) {
+                    e.printStackTrace();
+                }
             }
+
+            Toast.makeText(DrawingActivity.this, directory.getPath(),
+                    Toast.LENGTH_SHORT).show();
+
         });
     }
 
