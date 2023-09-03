fd57a6c31bffe37d2bc25b15f95e97eb824a237f
Wasura Wattearachchi <z5415828@ad.unsw.edu.au>
2023-09-03 15:04:18 +1000

    Add firebase login and register functionality

--- app/src/main/java/com/example/mapswarm/MainActivity.java
+++ app/src/main/java/com/example/mapswarm/MainActivity.java
@@ -6,30 +6,57 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button swarmButton;
-    private Button drawViewButton;
+    private Button swarmButton, drawViewButton, logoutButton;
+    private TextView welcomeText;
+    private FirebaseUser user;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        auth = FirebaseAuth.getInstance();
+        welcomeText = findViewById(R.id.welcomeText);
         swarmButton = findViewById(R.id.swarmButton);
+        drawViewButton = findViewById(R.id.drawViewButton);
+        logoutButton = findViewById(R.id.logoutBtn);
+        user = auth.getCurrentUser();
+
+        if (user == null) {
+             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+             startActivity(intent);
+             finish();
+        } else {
+            welcomeText.setText("Welcome "+ user.getEmail() + ", to SwarmTactiX!");
+        }
+
+        logoutButton.setOnClickListener(view -> {
+            auth.signOut();
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
         swarmButton.setOnClickListener(view -> {
             Intent intent = new Intent(view.getContext(), SwarmActivity.class);
             startActivity(intent);
+            finish();
         });
 
-        drawViewButton = findViewById(R.id.drawViewButton);
-        drawViewButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), QuestionnaireActivity.class);
-                startActivity(intent);
-            }
+
+        drawViewButton.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), QuestionnaireActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
---
+++ app/src/main/java/com/example/mapswarm/RegisterActivity.java
@@ -0,0 +1,99 @@
+package com.example.mapswarm;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextInputEditText usernameEditText, passwordEditText;
+    Button registerBtn;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    TextView loginTextView;
+    private final String TAG = "RegisterActivity";
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        registerBtn = findViewById(R.id.registerBtn);
+        progressBar = findViewById(R.id.progressBar);
+        loginTextView = findViewById(R.id.loginNow);
+
+        registerBtn.setOnClickListener(view -> {
+            progressBar.setVisibility(View.VISIBLE);
+            String username = String.valueOf(usernameEditText.getText());
+            String password = String.valueOf(passwordEditText.getText());
+
+            if (TextUtils.isEmpty(username)) {
+                Toast.makeText(RegisterActivity.this, "Enter username", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(RegisterActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            mAuth.createUserWithEmailAndPassword(username, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressBar.setVisibility(View.GONE);
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                Toast.makeText(RegisterActivity.this, "Registration successful",
+                                        Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(RegisterActivity.this, "Registration failed. " + task.getException().getLocalizedMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        });
+
+        loginTextView.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
