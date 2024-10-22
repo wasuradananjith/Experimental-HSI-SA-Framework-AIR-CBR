36d540c709cbfd62ed2aa515334c8dcab845593a
Wasura Wattearachchi <z5415828@ad.unsw.edu.au>
2024-10-23 10:06:38 +1100

    Degrade dim 6

--- app/src/main/java/com/example/mapswarm/SwarmActivity.java
+++ app/src/main/java/com/example/mapswarm/SwarmActivity.java
@@ -502,6 +502,8 @@ public class SwarmActivity extends AppCompatActivity implements OnMapReadyCallba
                         double[] robotPosition = new double[]{(double) locations.get(key).get(0),
                                 (double) locations.get(key).get(1)};
                         if (!deactivatedFlag && iconColour.equals(BitmapDescriptorFactory.HUE_BLUE)
+                                && !isInfDegradedDimMatched(Dims.Dim6.toString())
+                                && !isInfDegradedDimMatched(Dims.DimAll.toString())
                                 && isRobotTrapped(key, robotPosition)) {
                             iconColour = BitmapDescriptorFactory.HUE_YELLOW;
                         }
